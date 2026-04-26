249b0b6c8fa937b7600967b6a3833cc33e6fd6e4
honguyenhung <honguyennhung2010@gmail.com>
2026-04-27 00:06:39 +0700

    Fix deployment -  0:06:39.49

--- build-and-deploy.bat
+++ build-and-deploy.bat
@@ -3,25 +3,15 @@ echo Building and deploying NOPE CHAT...
 
 cd /d "%~dp0"
 
-echo [1/4] Building frontend...
-cd client
-call npm run build
-cd ..
-
-echo [2/4] Preparing server...
-cd server
-call npm install --production
-cd ..
-
-echo [3/4] Committing to git...
+echo [1/3] Committing current changes...
 git add .
-git commit -m "Auto build and deploy - %time%"
+git commit -m "Fix deployment - %time%"
 
-echo [4/4] Pushing to GitHub...
+echo [2/3] Pushing to GitHub...
 git push origin main
 
+echo [3/3] Triggering Vercel redeploy...
+echo Done! Check Vercel dashboard for deployment status.
 echo.
-echo Deploy completed! Sites will update in 2-3 minutes.
-echo Main: https://nhie.yennhie.site
-echo Admin: https://nhie.yennhie.site/admin
+echo If still broken, manually redeploy in Vercel dashboard.
 timeout /t 3
